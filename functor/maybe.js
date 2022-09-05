5a2e3129065204a5b2669382e09f392145fb556a
Nikita Nikitin <nickitin_2010@mail.ru>
2022-09-05 16:26:12 +0300

    refact

--- functor/maybe.js
+++ functor/maybe.js
@@ -1,6 +1,8 @@
-const {post, prefix} = require('../trash/increment.js');
+const {post: pInc, pre: prInc} = require('../trash/increment.js');
+const {post: pDec, pre: prDec} = require('../trash/decrement.js');
 
 const maybe = (x) => (fn) => maybe(x && fn ? fn(x) : null);
 
 maybe(5)(x => x**2)(console.log)
-maybe(10)(post)(prefix)(console.log)
+maybe(10)(prInc)(prInc)(prInc)(prDec)(prDec)(prDec)(console.log)
+maybe(10)(prDec)(prDec)(console.log)
